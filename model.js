72f1a11db509f6b0c256fd5042172130cc883240
Greiciane93x <greiciane_araujo@hotmai.com>
2020-11-18 16:43:05 -0300

    mais algumas alteracoes

--- model.js
+++ model.js
@@ -5,7 +5,6 @@ class UserModel{
         this.avatar_url = ""; 
         this.repos_url = ""; 
         this.url = "";  
-         
         
     }
     
@@ -15,57 +14,41 @@ class UserModel{
         let requisicao  = new XMLHttpRequest(); 
         requisicao.open("GET",  `https://api.github.com/users/` + login + `/repos`  , false)
         requisicao.addEventListener("load", () => {
+           try{
             if(requisicao.status == 200){
                 let objeto = this._processaResponse(requisicao.responseText); 
                 this._atualiza (objeto); 
+            }else if (requisicao.status == 400){
+                throw new Error("Usuário Inexistente")
             }
+        }catch(err){
+            console.log(err.message)
+        }
             
         })
         requisicao.send(); 
     }
     
-    // buscaRepositorio(){
-        
-    //     let login = this._login; 
-    //     let requisicao  = new XMLHttpRequest(); 
-    //     requisicao.open("GET",  `https://api.github.com/users/${login}/repos` , false)
-    //     requisicao.addEventListener("load", () => {
-          
-    //         if(requisicao.status == 200){
-               
-    //                 var objeto = this._processaResponse(requisicao.responseText); 
-                
-    //             this._atualiza(objeto); 
-    //         }
-            
-    //     })
-    //     requisicao.send(); 
-    // }
-    
-    
-    
+
     _processaResponse(responseString){
         let response = JSON.parse(responseString); 
         return response; 
     }
     
     _atualiza (dados){
-        
- 
-       
+              
         let montaUsuario = document.querySelector("body")
         let profileUsuario = document.createElement("div")
-
+       
+        
         montaUsuario.appendChild(profileUsuario)
         profileUsuario.innerHTML = `<img src=${dados[0].owner.avatar_url}>`
         for(let i of dados){
 
             let Repositorios = document.createElement("a"); 
-            
             montaUsuario.appendChild(Repositorios)
-
-            Repositorios.innerHTML =`
-            
+            Repositorios.innerHTML =
+            `
             <a href="${i.html_url}"></td>
             ${i.name}</a>
            
@@ -95,3 +78,17 @@ class UserModel{
     }
     
 }
+
+
+class UserController{
+    adicionaImagem(){
+        let dados = new UserModel(login.value)
+        dados.buscaImagem(); 
+        
+    }
+    
+}
+
+let controller = new UserController(); 
+document.getElementById("buscarRepositorio").addEventListener("click", controller.adicionaImagem)
+var login = document.querySelector("#search")
